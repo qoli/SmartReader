4e763358ef0f1d4b43dbe98ee50e62ee3003a66e
Ronnie W <llqoli@gmail.com>
2024-04-03 02:49:53 +0800

    修正了 css 錯誤

--- Shared (Extension)/Resources/content.js
+++ Shared (Extension)/Resources/content.js
@@ -242,11 +242,3 @@ function postProcessText(text) {
     .replaceAll("\n\n", "")
     .replaceAll(",,", "");
 }
-
-function vibratePhone(duration) {
-  if ("vibrate" in navigator) {
-    navigator.vibrate(duration);
-  } else {
-    console.log("该浏览器不支持震动功能");
-  }
-}
